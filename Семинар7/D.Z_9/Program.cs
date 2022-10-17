50d13ca03a43201936986f9a6a8b04a79a140be9
Denis <dfazylov@mail.ru>
2022-10-18 00:31:37 +0500

    Save

---
+++ Семинар7/D.Z_9/Program.cs
@@ -0,0 +1,85 @@
+﻿// // 
+void Zadacha64()
+{
+    // Задайте значения M и N. Напишите рекурсивный метод, 
+    // который выведет все натуральные числа кратные 3-ём в промежутке от M до N.
+    Console.WriteLine("Введите первое число");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число, оно должно быть больше чем первое ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+    FindNatural(n, m);
+}
+void FindNatural(int n, int a = 0)
+{
+    if( a % 3 == 0)
+    {
+    Console.WriteLine(a);
+    }
+    a++;
+    if (a > n)
+    {
+        return;
+    }
+    FindNatural(n, a);
+}
+Zadacha64();
+
+
+
+
+
+
+void Zadacha66()
+{
+    // Задайте значения M и N. Напишите программу, 
+    // которая найдёт сумму натуральных элементов в промежутке от M до N.
+    Console.WriteLine("Введите 1 число");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите 2 число");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int sum = m;
+    Console.WriteLine(SumNum(m, n, sum)); 
+    Console.WriteLine($"сумма чисел от числа {m} до {n} = {SumNum(m, n, sum)}");  
+}
+int SumNum(int m, int n, int sum)
+{ 
+    if (m >= n)
+    {
+        return sum;
+    }
+    m++;
+    sum = sum + m;
+    return SumNum(m, n, sum);
+}
+Zadacha66();
+
+
+
+
+
+
+
+
+void Zadacha68()
+{
+    Console.WriteLine("Введите 1 число");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите 2 число");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(Akkerman(m,n));
+    int Akkerman(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        if (m > 0 && n == 0)
+        {
+            return Akkerman(m - 1, 1);
+        }
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+
+    }
+}
+Zadacha68();
---
+++ Семинар7/Lesson_9/Program.cs
@@ -0,0 +1,101 @@
+﻿// 
+// void Zadacha63()
+// {
+//     // Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+//     // Решить с помошью рекурсии
+
+//     Console.WriteLine("Введите значение N");
+//     int n = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine();
+//     FindNatural(n);
+// }
+// void FindNatural(int n, int a = 0)
+// {
+//     a++;
+//     Console.WriteLine(a);
+//     if (a >= n)
+//     {
+//         return;
+//     }
+//     FindNatural(n, a);
+
+// }
+// Zadacha63();
+
+
+// void Zadacha65()
+// {
+//     // Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N
+//     Console.WriteLine("Введите значение N");
+//     int n = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Введите значение M");
+//     int m = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine();
+//     FindNatural(n, m);
+// }
+// void FindNatural(int n, int a = 0)
+// {
+
+//     Console.WriteLine(a);
+//     a++;
+//     if (a > n)
+//     {
+//         return;
+//     }
+//     FindNatural(n, a);
+
+// }
+// Zadacha65();
+
+
+// void Zadacha67()
+// {
+//     // Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+//     Console.WriteLine("Введите любое целое число");
+//     int num = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine(SumNum(num));
+// }
+// int SumNum(int num, int sum = 0)
+// {
+//     if (num == 0)
+//     {
+//         return sum;
+//     }
+//     sum = sum + num % 10;
+//     num = num / 10;
+//     return SumNum(num, sum);
+// }
+// Zadacha67();
+
+
+void Zadacha69()
+{
+    // Напишите программу которая на вход принимает два числа A и B, 
+    // и возводит число A в целую степень B с помощью рекурсии.
+    Console.WriteLine("Введите значение A");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите значение B");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+    Pow(a, b);
+}
+// void Pow(int a, int b, int result = 1) // Решение с помощью цикла
+// {
+//     for (int i = 0; i < b; i++)
+//     {
+//         result *= a;
+//     }
+//     Console.WriteLine($"{a}^{b} = {result}");
+// }
+void Pow(int a, int b, int i = 0, int result = 1) // Решение с помошью рекурсии
+{
+    if (i >= b)
+    {
+       Console.WriteLine($"{a}^{b} = {result}");
+       return; 
+    }
+    result *= a;
+    i++;
+    Pow(a, b, i, result);
+}
+Zadacha69();
